053ed388d3e23a5fa2d8ac519446f6a647f80f8d
andy <andy.william.mcdonald@gmail.com>
2019-08-08 12:37:50 +0100

    added-getData-js-query

---
+++ src/queries/getData.js
@@ -0,0 +1,8 @@
+const db_connection = require('../database/db_connection');
+
+const getBugbears = cb => {
+  db_connection.query('SELECT * FROM bugbears', (err, res) => {
+    if (err) return cb(err);
+    cb (null, res.rows);
+  });
+};
