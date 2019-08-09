e1daecd9f493ea8e5171958f79661ada33ec9341
Christine <christine.altmann@gmx.com>
2019-08-09 12:09:19 +0100

    simplified the pool function in connection.js

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -9,18 +9,15 @@ if (process.env.NODE_ENV === "test") {
 
 if (!DATABASE_URL) throw new Error("Environmental variable DB_URL must be set");
 
-const params = url.parse(DATABASE_URL);
-// console.log('THIS IS PARAMSSSS:', params);
-const [username, password] = params.auth.split(":");
+// const params = url.parse(DATABASE_URL);
+// // console.log('THIS IS PARAMSSSS:', params);
+// const [username, password] = params.auth.split(":");
 
-const options = {
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split("/")[1],
-  max: process.env.DB_MAX_CONNECTIONS || 2,
-  user: username,
-  password,
-  ssl: params.hostname !== "localhost"
-};
+// starsuit code recommendation
 
-module.exports = new Pool(options);
+let connectionString = process.env.DATABASE_URL;
+
+module.exports = new Pool({
+  connectionString,
+  ssl: !connectionString.includes("localhost")
+});
